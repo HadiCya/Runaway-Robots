8629e720352b3997a46ca54db116b961ebfd3cd5
JohnArvan <jjarvan2002@gmail.com>
2023-12-11 16:51:45 -0500

    Pause menu and main menu changes

---
+++ Assets/Scripts/PauseMenuButtons.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuButtons : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                UnpauseGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        pauseMenu.SetActive(true);
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
