df1f69160838b68fa1899c2117aa184b64eb0833
Daniel-T-Kline <danieltk@umich.edu>
2024-02-11 15:51:13 -0500

    Changed level gen
    walls, fences, and pits can now spawn close to the player.  robots are unchanged and must be at least 3 tiles away to spawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -555,14 +555,14 @@ public class GameManager : MonoBehaviour
         while (index < count)
         {
             spin_count += 1;
-            if (spin_count > 500)
+            if (spin_count > 5000)
             {
                 print("ERROR IN LEVEL GENERATION");
                 break;
             }
             int randrow = UnityEngine.Random.Range(1, board_size);
             int randcol = UnityEngine.Random.Range(1, board_size);
-            if (gameBoard[randrow, randcol] == null && !PlayerInProximity(randrow, randcol, 3) && CheckIfLegal(randrow, randcol, levelMap))
+            if (gameBoard[randrow, randcol] == null && (item.type != "robot" || !PlayerInProximity(randrow, randcol, 3)) && CheckIfLegal(randrow, randcol, levelMap))
             {
                 PlaceBoardItem(item, randrow, randcol);
                 levelMap[randrow, randcol] = 1;
